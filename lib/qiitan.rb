44e77f0b9135bbcea5ed078c36a5228f0573cc37
akihiro <akihiro.uesaka@gmail.com>
2014-05-07 19:44:13 +0900

    コメントアウトしている行を削除

--- lib/qiitan.rb
+++ lib/qiitan.rb
@@ -49,29 +49,12 @@ module Qiitan
 
 	class Client
 		def initialize(options)
-			#uri = URI.parse 'https://qiita.com/api/v1/auth'
-			#request = Net::HTTP::Post.new uri.request_uri
-			#request.set_form_data(options)
-			#http = Net::HTTP.new uri.host, uri.port
-			#http.use_ssl = true
-			##デバッグ時に使用する
-			##http.set_debug_output STDERR
-			#response = http.request(request)
-
-			#@token = JSON.parse(response.body)['token']
 			@rest = Qiitan::Rest.new
 			hashed = @rest.post 'https://qiita.com/api/v1/auth', true, options
 			@token = hashed['token']
 		end
 
 		def get_user_info
-			#uri = URI.parse API_BASE_URL + 'user?token=' + @token
-			#puts API_BASE_URL + 'user' + '/?token=' + @token
-			#request = Net::HTTP::Get.new uri.request_uri
-			#http = Net::HTTP.new uri.host, uri.port
-			#http.use_ssl = true
-			#response = http.request request
-			#json = JSON.parse response.body
 			@rest.get(API_BASE_URL + 'user', {'token' => @token })
 		end
 	end
